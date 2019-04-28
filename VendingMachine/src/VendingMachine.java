95a3184fddaf654aa149e649a8eb50c2f4ddf1c9
Yearn-TY <44322103+Yearn-TY@users.noreply.github.com>
2019-04-28 14:48:34 +0800

    VendingMachine
    VendingMachine

--- VendingMachine/src/VendingMachine.java
+++ VendingMachine/src/VendingMachine.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class VendingMachine {
 	private int price=120;
@@ -5,6 +6,7 @@ public class VendingMachine {
 	private int total;
 	void showPrompt(){
 		System.out.println("Welcome!");
+		System.out.println("You can chose 1,2,3,4 to insert monry,show balance,choose food and exit.");
 	}
 	public void insertMoney(int amount){
 		balance=balance+amount;
@@ -19,15 +21,34 @@ public class VendingMachine {
 			total=total+price;
 		}else{
 			System.out.println("Your balance is "+balance+" !");
+			System.out.println("You can choose 1 to insert money or 4 to exit.");
 		}
 	}
 	public static void main(String[] args) {
+		Scanner i=new Scanner(System.in);
 		VendingMachine vm=new VendingMachine();
 		vm.showPrompt();
-		vm.showBalance();
-		vm.insertMoney(100);
-		vm.getFood();
-		vm.showBalance();
+		int b=1,c;
+		while(b==1){
+			b=1;
+			c=i.nextInt();
+			switch(c){
+			case 1:
+				System.out.print("please input money:");
+				vm.insertMoney(i.nextInt());
+				break;
+			case 2:
+				vm.showBalance();
+				break;
+			case 3:
+				vm.getFood();
+				break;
+			case 4:
+				b=0;
+				break;
+			}
+		}
+		System.out.println("Bye!");
 	}
 
 }
