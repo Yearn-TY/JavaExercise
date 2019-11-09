b1af2302945aacddd4743f9214c7333f116b5195
Yearn-TY <44322103+Yearn-TY@users.noreply.github.com>
2019-11-09 20:59:41 +0800

    1

--- NameAgeAnnualIncome.java
+++
@@ -1,9 +0,0 @@
-public class NameAgeAnnualIncome{
-	
-	public static void main(String []args){
-		String name="hello";
-		int age=12;
-		double pay=100000.0;
-		System.out.println("My name is "+name+", my age is "+age+" and\n  I hope to earn $"+pay+" per year." );
-	}
-}
--- SalesPrediction.java
+++
@@ -1,7 +0,0 @@
-public class SalesPrediction{
-	public static void main(String []args){
-		double sales=4600000;
-		double percent=0.62;
-		System.out.println("East coast sales prediction: $"+sales*percent);
-	}
-}
